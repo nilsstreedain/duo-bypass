bc4827a6ae9e7d117a7a267b0051ed1e67fa2a73
Nils Streedain <tannins_berets_0@icloud.com>
2023-04-27 00:09:59 -0700

    Fixed domain

--- script/install.sh
+++ script/install.sh
@@ -9,7 +9,7 @@
 #	Created by Nils Streedain on 4/26/23.
 
 # Install Homebrew dependencies
-brew bundle --file=<(curl -fsSL https://raw.githubusercontent.com/nilsstreedain/duo-bypass/change-domain/script/Brewfile)
+brew bundle --file=<(curl -fsSL https://raw.githubusercontent.com/nilsstreedain/duo-bypass/main/script/Brewfile)
 
 # Install script to /usr/local/bin/ folder
 sudo curl https://raw.githubusercontent.com/nilsstreedain/duo-bypass/change-domain/script/duo-bypass -sSLo "/usr/local/bin/duo-bypass"
